c3582c141420f0793515e600af174847e48ed9db
Esther <bonjourjj3@gmail.com>
2024-09-06 17:52:37 +0900

    test6

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import App from "./App";
 import "dotenv/config";
 require("dotenv").config();
 
+const { APIKEY } = process.env;
+alert(APIKEY);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-const { APIKEY } = process.env;
-alert(APIKEY);
